7b30f25f9e4bdbd8ae0aa086b2d2d85f646f98d6
Eduardo Arroyo <dj.shadowjeab@gmail.com>
2022-11-09 22:34:42 -0500

    js intro

---
+++ 01 HTML Y CSS/04 agencia/js/codigo.js
@@ -0,0 +1,15 @@
+// DOM -> DOCUMENT OBJECT MODEL
+const nav = document.querySelector('.nav');
+// console.log(nav);
+// nav.classList.add('cambioDeNav');
+
+// console.log(window. pageYOffset);
+window.addEventListener("scroll", function(){
+    // console.log(window.pageYOffset);
+    if(window.scrollY > 0){
+        nav.classList.add('cambioDeNav');
+    }
+    else {
+        nav.classList.remove('cambioDeNav');
+    }
+});
