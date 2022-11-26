4f9c0609967b4c1d8db4ccf0208c6732633d6f2f
Eduardo Arroyo <dj.shadowjeab@gmail.com>
2022-11-26 18:36:26 -0500

    4 tables

--- 03 MYSQL Y PHP/01 MYSQL/02-relaciones.sql
+++ 03 MYSQL Y PHP/01 MYSQL/02-relaciones.sql
@@ -48,10 +48,59 @@ SELECT
     a.peli_restricciones
     FROM peliculas a, personajes b
         WHERE a.peli_id = b.per_peli_id
-        AND a.peli_restricciones = 'pg-13'
+        AND a.peli_restricciones = 'pg-13';
 
 -- 💡💡 3 TABLAS
 SELECT *
     FROM peliculas a, personajes b, actores c
     WHERE a.peli_id = b.per_peli_id
         AND b.per_act_id = c.act_id
+
+-- NOMBRES Y APELLIDOS DEL ACTOR | PERSONAJE | PELICULA | ORDENADO DESC FECHA ESTRENO
+
+SELECT
+    CONCAT(c.act_nombres, ' ', c.act_apellidos) AS actor,
+    b.per_nombre,
+    a.peli_nombre,
+    a.peli_estreno
+    FROM peliculas a, personajes b, actores c
+    WHERE a.peli_id = b.per_peli_id
+        AND b.per_act_id = c.act_id
+            ORDER BY a.peli_estreno DESC
+
+CREATE TABLE directores (
+    dire_id INT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    dire_nombres VARCHAR(50) NOT NULL,
+    dire_apellidos VARCHAR(50) NOT NULL
+)
+
+INSERT INTO directores (dire_nombres, dire_apellidos) VALUES
+    ('Jon', 'Watts'),
+    ('Lana', 'Wachowsky'),
+    ('James', 'Cameron'),
+    ('Christopher', 'Nolan'),
+    ('Stanley', 'Kubric'),
+    ('John', 'Krasinski'),
+    ('Martin', 'Campbell')
+
+ALTER TABLE peliculas ADD COLUMN peli_dire_id INT AFTER peli_id
+
+UPDATE peliculas SET peli_dire_id = 1 WHERE peli_id = 1
+UPDATE peliculas SET peli_dire_id = 2 WHERE peli_id = 2
+UPDATE peliculas SET peli_dire_id = 3 WHERE peli_id = 4
+UPDATE peliculas SET peli_dire_id = 3 WHERE peli_id = 8
+UPDATE peliculas SET peli_dire_id = 4 WHERE peli_id = 5
+UPDATE peliculas SET peli_dire_id = 4 WHERE peli_id = 9
+UPDATE peliculas SET peli_dire_id = 5 WHERE peli_id = 6
+UPDATE peliculas SET peli_dire_id = 6 WHERE peli_id = 10
+UPDATE peliculas SET peli_dire_id = 7 WHERE peli_id = 11
+
+SELECT *
+    FROM peliculas a, directores b
+        WHERE a.peli_dire_id = b.dire_id
+
+SELECT *
+    FROM peliculas a, personajes b, actores c, directores d
+    WHERE a.peli_id = b.per_peli_id
+        AND b.per_act_id = c.act_id
+        AND a.peli_dire_id = d.dire_id
