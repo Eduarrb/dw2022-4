934d0b166ce369f53a0b01436bdf62914d615082
Eduardo Arroyo <dj.shadowjeab@gmail.com>
2022-12-16 20:57:00 -0500

    sesion

--- 04 CMS/public/activate.php
+++ 04 CMS/public/activate.php
@@ -2,7 +2,7 @@
 <?php
     if(!isset($_GET['email']) || !isset($_GET['token'])){
         set_mensaje(display_msj("Datos de verificacion incorrectos", "danger"));
-        redirect("register.php");
+        redirect("login.php");
     } else {
         // echo 'todo ok';
         activar_usuario();
--- 04 CMS/resources/caller.php
+++ 04 CMS/resources/caller.php
@@ -3,4 +3,5 @@
     require_once("controllers/headerController.php");
     require_once("controllers/registerController.php");
     require_once("controllers/mailController.php");
+    require_once("controllers/loginController.php");
 ?>
